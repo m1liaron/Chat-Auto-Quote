7b05df2a76ec5ea766c7fbda74fe02657586d4d7
Vlad <machinker34@gmail.com>
2025-10-13 21:11:00 +0300

    feat: created auth middleWare, and validate env variables helper

--- backend/src/helpers/registerRoutes.ts
+++ backend/src/helpers/registerRoutes.ts
@@ -1,8 +1,12 @@
 import type { Application } from "express";
-import { authRoute } from "../routes/auth-route.js";
+import { authMiddleware } from "../middlewares/index.js";
+import { chatRoute, authRoute, userRoute, messageRoute } from "../routes/routes.js";
 
 const registerRoutes = (app: Application) => {
 	app.use("/auth", authRoute);
+	app.use("/chats", authMiddleware, chatRoute);
+	app.use("/users", authMiddleware, userRoute)
+	app.use("/messages", authMiddleware, messageRoute);
 };
 
 export { registerRoutes };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,13 @@
 import { EnvVariables } from "./common/enums/index.js";
 import { connectMongoDb } from "./db/connectMongoDB.js";
 import { app } from "./app.js";
+import { validateEnvVariables } from "./helpers/validateEnvVariables.js";
 
 const port = EnvVariables.PORT || 3000;
 
 const start = async () => {
 	try {
+		validateEnvVariables();
 		await connectMongoDb();
 		app.listen(port, () => {
 			console.log(`Server running on port http://localhost:${port}`);
